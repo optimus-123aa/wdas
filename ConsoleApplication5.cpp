72014a9ba71eb52b944a86a4440254bbf2be5266
optimus-123a <demonazazelx@gmail.com>
2019-11-28 22:30:22 +0200

    mhj

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,30 @@
+﻿#include "windows.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[6];
+
+	cout << "Block A:\n";
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> a[i];
+	}
+		for (int i = 1; i < 6; i += 2)
+	{
+		int t = a[i - 1];
+		a[i - 1] = a[i];
+		a[i] = t;
+	}
+
+	cout << "\n\nBlock C:\n";
+	for (int i = 0; i < 6; i++)
+	{
+		cout << a[i] << endl;
+	}
+	system("pause");
+	return 0;
+}
+
+
